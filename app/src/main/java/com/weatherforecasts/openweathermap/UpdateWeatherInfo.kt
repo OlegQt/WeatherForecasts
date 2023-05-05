32f3b9439bee9b40bc1f92391fa94c3d0db16d97
Oleg <60899063+OlegQt@users.noreply.github.com>
2023-05-05 23:05:00 +0300

    Revert "Dev retrofit"

--- app/src/main/java/com/weatherforecasts/openweathermap/UpdateWeatherInfo.kt
+++
@@ -1,7 +0,0 @@
-package com.weatherforecasts.openweathermap
-
-interface UpdateWeatherInfo {
-    fun inserCity(cityLocation: List<Cities>)
-    fun message(msg:String)
-    fun UpdateCurrentWeather(weather: CityWeather,cityName: String)
-}
